eee8fb40a14b59477afffc9fe241617f6bf5f1d3
Jun Yu (shockzinfinity) <shockzinfinity@gmail.com>
2021-06-24 11:14:31 +0900

    component test

---
+++ Client/Components/Home.razor.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace BlazorHostedIdentity.Client.Components
+{
+  public partial class Home
+  {
+    [Parameter] public string Title { get; set; }
+    [Parameter(CaptureUnmatchedValues = true)] public Dictionary<string, object> AdditionalAttributes { get; set; }
+    [CascadingParameter(Name = "HeadingColor")] public string Color { get; set; }
+  }
+}
