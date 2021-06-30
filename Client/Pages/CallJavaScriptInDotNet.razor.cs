aeca7e6686d8dd42e05a059d79a471df4e248b66
Jun Yu (shockzinfinity) <shockzinfinity@gmail.com>
2021-06-30 13:53:16 +0900

    js functions call

---
+++ Client/Pages/CallJavaScriptInDotNet.razor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorHostedIdentity.Client.Pages
+{
+  public partial class CallJavaScriptInDotNet
+  {
+    [Inject] public IJSRuntime JSRuntime { get; set; }
+
+    public async Task ShowAlertWindow()
+    {
+      await JSRuntime.InvokeVoidAsync("showAlert", "JS function called from .NET");
+    }
+  }
+}
