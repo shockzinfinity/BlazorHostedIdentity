9bf63080e70a9aa1f84722ac892ada2dc8661873
Jun Yu (shockzinfinity) <shockzinfinity@gmail.com>
2021-07-15 18:50:54 +0900

    add details

---
+++ Client/Components/AdditionalProductInfo.razor.cs
@@ -0,0 +1,12 @@
+﻿using BlazorHostedIdentity.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorHostedIdentity.Client.Components
+{
+  public partial class AdditionalProductInfo
+  {
+    [Parameter] public Product Product { get; set; }
+    [Parameter] public int ReviewCount { get; set; }
+    [Parameter] public int QuestionCount { get; set; }
+  }
+}
---
+++ Client/Components/QuestionsAnswers.razor.cs
@@ -0,0 +1,11 @@
+﻿using BlazorHostedIdentity.Shared;
+using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+
+namespace BlazorHostedIdentity.Client.Components
+{
+  public partial class QuestionsAnswers
+  {
+    [Parameter] public ICollection<QA> QAs { get; set; }
+  }
+}
