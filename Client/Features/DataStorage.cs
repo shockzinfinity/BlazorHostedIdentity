27c73469f264ce1f610502137a0ecbd051fe2949
Jun Yu (shockzinfinity) <shockzinfinity@gmail.com>
2021-07-15 19:04:28 +0900

    add chart

---
+++ Client/Features/DataStorage.cs
@@ -0,0 +1,22 @@
+﻿using MudBlazor;
+using System.Collections.Generic;
+
+namespace BlazorHostedIdentity.Client.Features
+{
+  public static class DataStorage
+  {
+    public static (List<ChartSeries> ChartData, string[] Labels) GetChartData(string productName)
+    {
+      var chartData = new List<ChartSeries> {
+        new ChartSeries {
+          Name = productName,
+          Data = new double[] {23, 35, 52, 46, 40, 50}
+        }
+      };
+
+      var labels = new string[] { "Jan", "Feb", "Mar", "Apr", "May", "Jun" };
+
+      return (chartData, labels);
+    }
+  }
+}
